6f9dc4ca4efe09d27abc9cb7f82bf9dbd5a364dd
REUTSKIY YURIY <yurareutskiy@gmail.com>
2021-03-10 18:47:25 +0300

    Initialize repo; Add flask project with uploader;

---
+++ s3_uploader.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+access_key = os.getenv("AWS_ACCCESS_KEY")
+secret_key = os.getenv("AWS_SECRET_KEY")
+
+import boto3
+
+
+def uploadPublicFile(path, folder = ""):
+    s3 = boto3.client('s3')
+    filename = (path.split("/"))[-1]
+    if folder == "":
+        finalPath = filename
+    else:
+        finalPath = folder + "/" + filename
+    response = s3.upload_file(
+        path, "files-work", finalPath,
+        ExtraArgs={'ACL': 'public-read'}
+    )
+    print("File uploding: ", response)
+
+# uploadPublicFile("cat.png", "bot_test")
